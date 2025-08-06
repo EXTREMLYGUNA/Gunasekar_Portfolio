0d8ea2941143df5c2ef42a1f0a192e14e5e39ff9
GUNASEKAR <gunasekar17329777557amma@gmail.com>
2025-08-06 20:31:56 +0530

    updated-portfolio

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -5,9 +5,8 @@ function About() {
          <p>I am a passionate developer turning ideas into real-world solutions.</p>
          <a title="Tasks" href="https://gunasekar-resume.netlify.app/" target="_blank" className="resume" rel="noopener"><h3>Resume</h3></a>
          <a href='https://gunasekar.netlify.app/assets/GUNASEKAR.pdf' download className="resume">
-             <h3 style={{display:"inline", padding: "13px"}}>Download My Resume (PDF)</h3>
+             <h3 style={{ padding: "10px",width:'165px'}}>Download(PDF)</h3>
         </a>
-
     </section>
     </>)
 }
