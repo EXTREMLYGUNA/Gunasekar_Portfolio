99d0d2fa15837d8ffcafe6dc9eccce7bf0656a42
GUNASEKAR <gunasekar17329777557amma@gmail.com>
2025-08-06 21:15:34 +0530

    updated-portfolio

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -4,9 +4,12 @@ function About() {
          <h2>About Me</h2>
          <p>I am a passionate developer turning ideas into real-world solutions.</p>
          <a title="Tasks" href="https://gunasekar-resume.netlify.app/" target="_blank" className="resume" rel="noopener"><h3>Resume</h3></a>
-         <a href='https://gunasekar.netlify.app/assets/GUNASEKAR.pdf' download className="resume">
-             <h3 style={{ padding: "10px",width:'165px'}}>Download(PDF)</h3>
-        </a>
+         <a href='https://gunasekar.netlify.app/assets/GUNASEKAR.pdf' 
+            download 
+            className="resume"
+            target="_blank" 
+            rel="noopener">
+             <h3 style={{ padding: "10px",width:'165px'}}>Download(PDF)</h3></a>
     </section>
     </>)
 }
--- src/Components/Links.jsx
+++ src/Components/Links.jsx
@@ -4,7 +4,7 @@ function Links() {
       <a href='https://www.linkedin.com/in/gunasekar-v-0a1054288' className='LinkedIn' target="_blank" rel="noopener" ><img title="LinkedIn" src='https://creazilla-store.fra1.digitaloceanspaces.com/icons/7912623/linkedin-icon-md.png' style={{height: "58px", width: "58px", marginBottom:"-3px"}}/></a>
       <a href='https://github.com/EXTREMLYGUNA' className='Github' target="_blank" rel="noopener" >                    <img title="Github" src='https://cdn-icons-png.flaticon.com/512/25/25231.png' style={{height: "50px", width: "50px", marginLeft: "10px"}} /></a>
   </section>
-      <p>&copy; 2025 </p>
+      <p> &copy; {new Date().getFullYear()}   </p>
     </>)
 }
 
